343ef377810f14b5e8e89ef6737194dfbfba1d05
MutwakilX <144057106+Wadamzmail@users.noreply.github.com>
2025-03-09 11:42:31 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -44,7 +44,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 
 SESSION_ID: process.env.SESSION_ID === undefined ? 'Manu-MD&JXkyHSAQ#xp0Y171w7Qt2BzKhwNbfpxV4jQEQ7y_pFJaCwY4QbIw' : process.env.SESSION_ID,
-PREFIX: process.env.PREFIX || '.' ,
+PREFIX: process.env.PREFIX || '*' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '94742274855' : process.env.SUDO,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
